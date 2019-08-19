54c00bb7490a62904dcb61d0cbb330a0fe301e4c
OleksiiKanivets <Oleksii.kanivets@smartbear.com>
2019-08-19 15:07:49 +0300

    test

--- src/main/java/test/testIntegr.java
+++ src/main/java/test/testIntegr.java
@@ -1,5 +1,5 @@
 package test;
 
 public class testIntegr {
-    //2341234trewrtweertwertwerterqwerqweqrqwerqwerfghjxcvbnmgfjhgjsdASDas
+    //2341234trewrtweertwertwerterqwerqweqrqwerqwerfghjxcvbnmgfjhgjsdASDasывафыва
 }
