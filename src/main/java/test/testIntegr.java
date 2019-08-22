487cedc83cd312594b85f62ed2a284f7325b8c79
OleksiiKanivets <Oleksii.kanivets@smartbear.com>
2019-08-22 12:13:01 +0300

    test

--- src/main/java/test/testIntegr.java
+++ src/main/java/test/testIntegr.java
@@ -2,5 +2,5 @@ package test;
 
 public class testIntegr {
     //2341234trewrtweertdfasdfasfsadfsadfwertwerterqwerasdfasdqweqrqwerqwerfghjxcvbnrtyurumgfjhgjsdASDagdfghghjkghjkghjkdfgsывафывакенгкенгкенгвапывапк'
-    // ;';'werqweraefaswdfasdfеаонЯЧсЯЧсгрлдукецукецукевыапывапвыапыertyeвапыrtyertyвапервапрвапячсмячсмчсмячсмч
+    // ;';'werqweraefaswdfasdfеаонЯЧсЯЧсгрлдукецукецукевыапывапвыапыertyeqwerqwerвапыrtyertyвапервапрвапячсмячсмчсмячсмч
 }
